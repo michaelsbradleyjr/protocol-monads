ba68a3b78972052599d0846ebe131ed65b7c7f66
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2012-11-02 16:03:36 -0500

    note re: 'dummy value' [nil] as used in monads.core/do macro

--- src/monads/core.clj
+++ src/monads/core.clj
@@ -63,26 +63,30 @@
    a vector of steps given as binding-form/monadic-expression pairs, and
    a result value specified by expr. The monadic-expression terms can use
    the binding variables of the previous steps.
+
    If the monad contains a definition of m-zero, the step list can also
    contain conditions of the form :when p, where the predicate p can
    contain the binding variables from all previous steps.
+
    A clause of the form :let [binding-form expr ...], where the bindings
    are given as a vector as for the use in let, establishes additional
-   bindings that can be used in the following steps."
-  [result bindings expr]
+   bindings that can be used in the following steps. "
+  [monad bindings expr]
   (let [steps (partition 2 bindings)]
-    `(monads.core/bind (~result [nil])
+    ;; The "dummy value" [nil] is used in several expressions below to
+    ;; couple calls to bind and zero to the appropriate protocol-monad
+    `(monads.core/bind (~monad [nil])
                        (fn [_#]
                          ~(reduce (fn [expr [sym mv]]
                                     (cond
                                       (= :when sym) `(if ~mv
                                                        ~expr
-                                                       (monads.core/zero (~result [nil])))
+                                                       (monads.core/zero (~monad [nil])))
                                       (= :let sym) `(let ~mv
                                                       ~expr)
                                       :else `(monads.core/bind ~mv (fn [~sym]
                                                                      ~expr))))
-                                  `(monads.core/do-result (~result [nil]) ~expr)
+                                  `(monads.core/do-result (~monad [nil]) ~expr)
                                   (reverse steps))))))
 
 (defn- comprehend [f mvs]
