e31967f4d50d76f5cacd6b898b022352a532f7de
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2012-11-18 05:28:59 -0600

    more examples ported

--- src/monads/examples.clj
+++ src/monads/examples.clj
@@ -182,48 +182,83 @@
 
 
 (defex ex15
-  (let [gaussian (m/do m/state
-                       [x1  rng
-                        x2  rng
-                        x3  rng
-                        x4  rng
-                        x5  rng
-                        x6  rng
-                        x7  rng
-                        x8  rng
-                        x9  rng
-                        x10 rng
-                        x11 rng
-                        x12 rng]
-                       (- (+ x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12) 6.))]
+  (let [gaussian
+        (m/do m/state
+              [x1  rng
+               x2  rng
+               x3  rng
+               x4  rng
+               x5  rng
+               x6  rng
+               x7  rng
+               x8  rng
+               x9  rng
+               x10 rng
+               x11 rng
+               x12 rng]
+              (- (+ x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12) 6.))]
     [(symbol "mean =>") (mean (take 1000 (value-seq gaussian 1)))
      (symbol "variance =>") (variance (take 1000 (value-seq gaussian 1)))]))
 
 
 (defex ex16
-  (let [gaussian (m/do m/state
-                       [sum12 (reduce (m/lift +) (repeat 12 rng))]
-                       (- sum12 6.))]
+  (let [gaussian
+        (m/do m/state
+              [sum12 (reduce (m/lift +) (repeat 12 rng))]
+              (- sum12 6.))]
     [(symbol "mean =>") (mean (take 1000 (value-seq gaussian 1)))
      (symbol "variance =>") (variance (take 1000 (value-seq gaussian 1)))]))
 
 
 (defex ex17
-  (let [gaussian ((m/lift #(- % 6.))
-                  (m/reduce + (repeat 12 rng)))]
+  (let [gaussian
+        ((m/lift #(- % 6.))
+         (m/reduce + (repeat 12 rng)))]
     [(symbol "mean =>") (mean (take 1000 (value-seq gaussian 1)))
      (symbol "variance =>") (variance (take 1000 (value-seq gaussian 1)))]))
 
 
+(defex ex18
+  (let [rng2 (m/seq [rng rng])]
+    (rng2 1)))
 
 
+(defex ex19
+  (let [identical-random-seqs
+        (m/do m/state
+              [seed (m/get-state)
+               x1   rng
+               x2   rng
+               _    (m/set-state seed)
+               y1   rng
+               y2   rng]
+              (list [x1 x2] [y1 y2]))]
+    (identical-random-seqs 1)))
 
 
+(defex ex20
+  (let [writer+str (m/writer "")
+        write (fn [msg] (m/write-writer writer+str msg))]
+    @(m/do writer+str
+           [x (writer+str 1)
+            _ (write "first step\n")
+            y (writer+str 2)
+            _ (write "second step\n")]
+           (+ x y))))
 
 
-
-
-
-
-
-
+(defex ex21
+  (let [writer+vec (m/writer [])
+        write (fn [msg] (m/write-writer writer+vec msg))
+        fib-trace (fn fib-trace [n]
+                    (if (< n 2)
+                      (writer+vec n)
+                      (m/do writer+vec
+                            [n1 (writer+vec (dec n))
+                             n2 (writer+vec (dec n1))
+                             f1 (fib-trace n1)
+                             _  (write [n1 f1])
+                             f2 (fib-trace n2)
+                             _  (write [n2 f2])]
+                            (+ f1 f2))))]
+    @(fib-trace 5)))
