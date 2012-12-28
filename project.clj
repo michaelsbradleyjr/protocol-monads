a47caada95d485093a913f7f6dab8f9120e8752e
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2012-12-28 02:04:45 -0600

    indicate license in project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject monads "1.0.0"
   :description "A protocol based monads library for Clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.4.0"]])
